8507515b6c05698d0a87d4634f093b05a9615dab
flyfly <fly@fly.com>
2020-10-12 10:33:10 +0800

    raw-serial-test

---
+++ CMakeFiles/factorytest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factorytest.dir/factorytest.c.o"
+  "factorytest.pdb"
+  "factorytest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/factorytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
